652281bd12dfc3e02effa8a0e01b77e2b8bb063a
Castro Fernandez, Blas <bcastrof@indra.es>
2021-02-07 16:35:09 +0100

    users ws with active HTTPS

---
+++ demo-Users-ws/src/main/java/es/myorg/app/api/users/ApiUsersApplication.java
@@ -0,0 +1,13 @@
+package es.myorg.app.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsersApplication.class, args);
+	}
+
+}
